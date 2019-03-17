95c2e99d7541bc030f1e81004101b72a34be7be7
Bartosz Sowa <b.sowa9@gmail.com>
2019-03-17 18:10:43 +0100

    Add simple PlayerController

--- MetalGearKrisow/Assets/Scripts/PlayerController.cs
+++ MetalGearKrisow/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator Animator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalMove = Input.GetAxis("Horizontal");
+        Animator.SetFloat("speed", Mathf.Abs(horizontalMove));
     }
 }
