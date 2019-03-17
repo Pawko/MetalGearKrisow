173cdfbc9bdeddd631b133bdb81d669202b86e68
Paweł Wasil <pawel.hr@gmail.com>
2019-03-17 21:01:26 +0100

    Added a script to follow the hero

---
+++ MetalGearKrisow/Assets/Scripts/FollowingHero.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingHero : MonoBehaviour
+{
+    public GameObject hero;
+    private Vector3 currVelocity;
+    public float smoothTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPosition = new Vector3(hero.transform.position.x, hero.transform.position.y, transform.position.z);
+
+        transform.position = Vector3.SmoothDamp(transform.position, cameraPosition, ref currVelocity, smoothTime);
+    }
+}
