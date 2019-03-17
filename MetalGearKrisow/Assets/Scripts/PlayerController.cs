4127e976caaae6e5dbf7a8e36401d4b4639fe8a8
Bartosz Sowa <b.sowa9@gmail.com>
2019-03-17 18:31:17 +0100

    Add simple player movement  (left + right)

--- MetalGearKrisow/Assets/Scripts/PlayerController.cs
+++ MetalGearKrisow/Assets/Scripts/PlayerController.cs
@@ -2,18 +2,42 @@
 
 public class PlayerController : MonoBehaviour
 {
+    public float PlayerSpeed;
+
     private Animator Animator;
+    private Rigidbody2D Rigidbody2D;
+    private bool IsDirectionToRight = true;
     
     // Start is called before the first frame update
     void Start()
     {
         Animator = GetComponent<Animator>();
+        Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontalMove = Input.GetAxis("Horizontal");
+        Rigidbody2D.velocity = new Vector2(horizontalMove * PlayerSpeed, Rigidbody2D.velocity.y);
+
         Animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+        if (horizontalMove < 0 && IsDirectionToRight) // Left Arrow was clicked
+        {
+            Flip();
+        }
+        if (horizontalMove > 0 && !IsDirectionToRight) // Right Arrow was clicked
+        {
+            Flip();
+        }        
+    }
+
+    private void Flip()
+    {
+        IsDirectionToRight = !IsDirectionToRight;
+        Vector3 playerScale = gameObject.transform.localScale;
+        playerScale.x *= -1;
+        gameObject.transform.localScale = playerScale;
     }
 }
