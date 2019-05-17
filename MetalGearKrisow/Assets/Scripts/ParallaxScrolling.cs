d46cb25f41bd575502adeebd59399f19fda8d542
Bartosz Sowa <b.sowa9@gmail.com>
2019-05-17 20:51:50 +0200

    Added Parallax Scrolling

---
+++ MetalGearKrisow/Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public float parallaxFactor;
+
+    private Vector3 previousCameraPosition;
+    private Vector3 deltaCameraPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousCameraPosition = cameraTransform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaCameraPosition = cameraTransform.position - previousCameraPosition;
+        Vector3 parallaxPosition = new Vector3(
+            x: transform.position.x + (deltaCameraPosition.x * parallaxFactor), 
+            y: transform.position.y, 
+            z: transform.position.z
+        );
+        transform.position = parallaxPosition;
+        previousCameraPosition = cameraTransform.position;
+    }
+}
