7a0f8348cf51da4e14e3b822139bfdc2f81bdac6
Bartosz Sowa <b.sowa9@gmail.com>
2019-05-17 21:53:00 +0200

    Fail Cactus Controller

---
+++ MetalGearKrisow/Assets/Scripts/FailCactusController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailCactusController : MonoBehaviour
+{
+    public Transform startPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Girl")
+        {
+            other.transform.position = startPoint.position;
+        }
+    }
+}
