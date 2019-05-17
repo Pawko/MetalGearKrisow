f5ccffa1d61ece51ff30c28cf2699d1aa613dad2
Bartosz Sowa <b.sowa9@gmail.com>
2019-05-18 00:10:50 +0200

    4.5 - Pick Up Box

---
+++ MetalGearKrisow/Assets/Scripts/PickUpBoxController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpBoxController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Girl")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
