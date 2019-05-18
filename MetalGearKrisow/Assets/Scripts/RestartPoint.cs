893606483a2592177842c9eb204f4aacec6caddc
Bartosz Sowa <b.sowa9@gmail.com>
2019-05-18 23:30:49 +0200

    Restart Point

---
+++ MetalGearKrisow/Assets/Scripts/RestartPoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class RestartPoint : MonoBehaviour
+{
+    private RestartPointsManager restartPointsManager;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        restartPointsManager = GameObject.Find("Manager").GetComponent<RestartPointsManager>();
+        if (restartPointsManager == null)
+        {
+            Debug.LogError("RestartPointsManager is null.");
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            restartPointsManager.UpdateStartPoint(this.gameObject.transform);
+            spriteRenderer.color = new Color(0.3f, 0.8f, 0.6f);
+        }
+
+    }
+}
---
+++ MetalGearKrisow/Assets/Scripts/RestartPointsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartPointsManager : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void UpdateStartPoint(Transform restartPoint)
+    {
+        playerController.startPoint = restartPoint;
+    }
+}
