f6961be29383ffa21838c31b48157743e7de3423
Bartosz Sowa <b.sowa9@gmail.com>
2019-05-22 00:27:37 +0200

    Fixed picking up box

--- MetalGearKrisow/Assets/Scripts/PickUpBoxController.cs
+++ MetalGearKrisow/Assets/Scripts/PickUpBoxController.cs
@@ -7,9 +7,12 @@ public class PickUpBoxController : MonoBehaviour
     CounterController counterController;
     public AudioClip clip;
     public GameObject particlesPrefab;
+
+    private bool isCurrentlyTriggered;
     // Start is called before the first frame update
     void Start()
     {
+        isCurrentlyTriggered = false;
         counterController = GameObject.Find("Manager").GetComponent<CounterController>();
         if (counterController == null)
             Debug.LogError("CounterController is null");
@@ -17,12 +20,21 @@ public class PickUpBoxController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "Girl")
+        if (!isCurrentlyTriggered && other.gameObject.tag == "Player")
         {
+            isCurrentlyTriggered = true;
             Destroy(this.gameObject);
             AudioSource.PlayClipAtPoint(clip, transform.position);
             counterController.IncrementCounter();
             Instantiate(particlesPrefab, transform.position, transform.rotation);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isCurrentlyTriggered = false;
         }
     }
 }
